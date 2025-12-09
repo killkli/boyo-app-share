d37645ccaf6b003cb24fb23aef72221a059bcb37
killkli <dchensterebay@gmail.com>
2025-12-09 11:41:36 +0800

    feat: allow blob URLs in the frame-src Content Security Policy directive

--- server/middleware/security.ts
+++ server/middleware/security.ts
@@ -16,7 +16,7 @@ export default defineEventHandler((event) => {
       "img-src 'self' data: https: blob:",
       "font-src 'self' data:",
       "connect-src 'self' https://s3.tebi.io",
-      "frame-src 'self'",
+      "frame-src 'self' blob:",
       "object-src 'none'",
       "base-uri 'self'",
       "form-action 'self'",
